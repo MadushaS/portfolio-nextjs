1f84184ca9569220e4fe22da8df758dd7421e1fc
_MadushaSandaruwan <madushakv@live.com>
2024-07-13 23:43:52 +0530

    fixed homepage metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import SmoothLoader from "@/components/SmoothLoader"
-import Container from "@/components/Home/HomePage"
+import HomePageContainer from "@/components/Home/HomePage"
 
 export const metadata = {
-  title: "Home",
-  description: "A personal portfolio website built with Next.js and Tailwind CSS.",
-  image: "/placeholder.svg",
-  date: "2022-01-01",
-  tags: ["next.js", "tailwindcss"],
+  title: "Madusha Sandaruwan - Portfolio Website",
+  description: "Madusha is a GitHub certified full-stack developer who loves to build things that live on the internet. He develops exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.",
 }
 
 export default function Component() {
   return (
     <div className="flex flex-col min-h-screen">
       <SmoothLoader />
-      <Container />
+      <HomePageContainer />
     </div>
   )
 }
